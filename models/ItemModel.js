9d67db500436ae7eb25a39172414bc7a260c7b1e
MattTheMicrowave <mwlluis@hotmail.com>
2016-05-23 13:57:07 -0500

    app is working

--- models/ItemModel.js
+++ models/ItemModel.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+// this is set up for 2 attributes
 var itemSchema = new Schema({
 
   'verdura' : String,
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var ItemModel = require('../models/ItemModel.js');
 
-/* GET home page. */
+/* GET home page. this will list all of the items in the db*/
 router.get('/', function(req, res, next) {
   ItemModel.find(function(err, items) {
   res.render('index', { title: 'Express',
